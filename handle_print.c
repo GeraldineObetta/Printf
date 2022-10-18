1f5204b81635dbcd3a80ae4c01a1f2762151c7e1
Mrfamzy1 <111088104+Mrfamzy1@users.noreply.github.com>
2022-10-18 01:02:04 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -9,7 +9,7 @@
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
- * Return: 1 or 2;
+ * Return: 1 or 2
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
